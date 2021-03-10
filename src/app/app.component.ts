22d81c0bb863d7eba19efadd5570a058decd1c78
Musharraf Hussain <56526948+Musarraf-Hussain@users.noreply.github.com>
2021-03-10 23:20:31 +0500

    loginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'login', url: '/login', icon: 'mail' },
+    { title: 'Signup', url: '/signup', icon: 'mail' },
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}

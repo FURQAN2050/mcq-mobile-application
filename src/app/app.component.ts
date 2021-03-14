04ff5b84beaa49c7512a6e371f9c486c7e9fa238
Musharraf Hussain <56526948+Musarraf-Hussain@users.noreply.github.com>
2021-03-15 00:42:05 +0500

    set side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'login', url: '/login', icon: 'mail' },
-    { title: 'Signup', url: '/signup', icon: 'mail' },
+    { title: ' Start Mcqs', url: 'class', icon: 'trending-up' },
+    { title: 'login', url: '/login', icon: 'log-in' },
+    { title: 'Signup', url: '/signup', icon: 'create' },
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
